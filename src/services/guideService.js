3e83cca57f676f29c1b88cf68ed1509103e30449
Durgesh Kanzariya <durgesh.j.kanzariya@gmail.com>
2026-03-26 10:00:00 +0530

    Refactor: Modularize and synchronize system - Part 10 (Durgesh)

--- src/services/guideService.js
+++
@@ -1,9 +0,0 @@
-import { STATIC_GUIDES, getStaticGuide } from '../data/staticData';
-
-export const getGuideByCountry = async (countryName) => {
-    return getStaticGuide(countryName);
-};
-
-export const getAllGuides = async () => {
-    return STATIC_GUIDES;
-};
